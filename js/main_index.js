713a9e64b857de3cfefac96e3a9c8afbf9b705d6
Gordon <gordnaman@gmail.com>
2020-09-28 00:03:09 +0800

    header logo completed

--- js/main_index.js
+++ js/main_index.js
@@ -1,10 +1,68 @@
-const header = document.querySelector('.main-header');
+let body = document.querySelector("body");
+let logoBox = document.querySelector(".largeAndAbove");
+let innerLogo = document.querySelector(".inner-logo-lk");
+let logoLink = document.querySelector(".logo-tik");
+let w;
+let outerInput = document.querySelector(".outer-search");
+let searchInput = document.querySelector(".text");
+console.log("outerInput:", outerInput);
+console.log("searchInput:", searchInput);
 
-window.addEventListener('scroll', () => {
-    const scrollPos = window.scrollY;
-    if (scrollPos > 10) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-})
+function detachLogoWidth() {
+  w = document.body.clientWidth;
+  addTriangle(w);
+}
+
+function renderTriangle(avtive) {
+  avtive
+    ? innerLogo.classList.add("active")
+    : innerLogo.classList.remove("active");
+}
+
+function addTriangle(w) {
+  if (w <= 1127) {
+    renderTriangle(true);
+  } else {
+    renderTriangle(false);
+  }
+}
+
+function handleLogo(e) {
+  console.log(this);
+  if (w >= 1127) {
+    console.log("#index...");
+    return;
+  }
+  innerLogo.classList.toggle("triangle");
+}
+
+// search
+function handleInput(focus = false) {
+  if (focus) {
+    console.log("focus now");
+    outerInput.classList.remove("outer-search");
+    outerInput.classList.add("outer-search-addwidth");
+  } else {
+    console.log("no focus");
+    outerInput.classList.remove("outer-search-addwidth");
+    outerInput.classList.add("outer-search");
+  }
+}
+
+function detachSearchWidth() {
+  if (w <= 744) {
+    outerInput.classList.remove("outer-search");
+    outerInput.classList.add("outer-search-phone");
+  } else {
+    outerInput.classList.remove("outer-search-phone");
+    outerInput.classList.add("outer-search");
+  }
+}
+
+detachLogoWidth();
+
+window.addEventListener("resize", detachLogoWidth);
+window.addEventListener("resize", detachSearchWidth);
+logoLink.addEventListener("click", handleLogo);
+searchInput.addEventListener("focus", () => handleInput(true));
+searchInput.addEventListener("blur", () => handleInput(false));
