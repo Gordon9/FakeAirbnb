6629792a31946281ca538a4b3c4cfb014b9bb929
Gordon <gordnaman@gmail.com>
2020-10-16 00:05:53 +0800

    fx select basic style complete but no js

--- js/main_index.js
+++ js/main_index.js
@@ -203,16 +203,16 @@ function handleBtnClick() {
     let makeDiv = document.createElement("div");
     makeDiv.innerHTML = `<div class='_btn-fy-outer'>
               <button type='button' class='_btn-fy-default' aria-busy='false'>
-                <span>房源</span>
+                <span class='_fy-span'>房源</span>
                 <div>
                 <svg viewBox="0 0 24 24" role="img" aria-hidden="false" aria-label="Expand" focusable="false" style="height: 1em; width: 1em; display: block; fill: currentcolor;"><path d="m12.5 17c-.4426799 0-.8662787-.1919807-1.1664869-.5259747l-6.9098769-7.71341826c-.59660019-.66535794-.55843813-1.70547282.0852286-2.3221781.6449388-.61802022 1.65241717-.57725718 2.24774529.08810076l5.74338991 6.4103161 5.7433899-6.4103161c.5953281-.66404301 1.6015344-.70612098 2.2477453-.08810076.6436667.61670528.6818288 1.65682016.0852286 2.3221781l-6.9098769 7.71341826c-.3002082.333994-.723807.5259747-1.1664869.5259747"></path></svg>
                 </div>
               </button>
               <button type='button' class='_btn-fy-lk' aria-busy='false'>
-                <span>房源</span>
+                <span class='_fy_span-middle'>房源</span>
               </button>
               <button type='button' class='_btn-ty-lk' aria-busy='false'>
-                <span>体验</span>
+                <span class='_btn-span-bottom'>体验</span>
               </button>
     </div>`;
 
@@ -234,8 +234,8 @@ function handleBtnClick() {
       let btnFyLkParent = btnFyLk.parentNode.parentNode;
       console.log("btnFyLkParent:", btnFyLkParent);
 
-      btnFyLkParent.remove(fyWrapper);
-      makeFxBtn();
+      // btnFyLkParent.remove(fyWrapper);
+      // makeFxBtn();
     });
   });
 
